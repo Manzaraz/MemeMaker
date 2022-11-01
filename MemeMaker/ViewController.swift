167ced5d391299e788bdbccfb0433e6b89a1d1b9
Christian Manzaraz <magnusmanz@gmial.com>
2022-11-01 18:33:09 -0300

    Initial Commit

---
+++ MemeMaker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemeMaker
+//
+//  Created by Christian Manzaraz on 01/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
